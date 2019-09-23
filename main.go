f4e25b921b6a7f0318a61ffe22604ec7f2abc16f
Vladimir DOMBROVSKI <vladimir@openio.io>
2019-09-23 23:47:38 +0200

    Phase3: WIP: Initialize routes for project/user/key creation

--- main.go
+++ main.go
@@ -9,23 +9,198 @@ import (
     "crypto/sha256"
     "encoding/hex"
     "fmt"
+    "encoding/json"
+    "oioiam/oio"
+    "math/rand"
+    "time"
 )
+
+var seed = []byte("changeme")
+const acVersion = "1"
 
 const respTpl = "{\"token\":{\"roles\":[]," +
 "\"project\":{\"domain\":{\"id\":\"\",\"name\":\"\"},\"id\":\"%s\",\"name\":\"%s\"}," +
 "\"user\":{\"domain\":{\"id\":\"\",\"name\":\"\"},\"id\":\"\",\"name\":\"%s\"}}}"
 
+type project struct {
+    Project *string `json:"project"`
+}
+
+type user struct {
+    Project *string `json:"project"`
+    User *string `json:"user"`
+}
+
+type key struct {
+    Project *string `json:"project"`
+    User *string `json:"user"`
+    Access *string `json:"access"`
+    Secret *string `json:"secret"`
+}
+
+type httpIface struct {
+    PC *oio.ProxyClient
+}
+
+func makeHTTPIface() *httpIface {
+    return &httpIface{
+        PC: oio.MakeProxyClient("http://10.10.10.11:6006", "OPENIO"),
+    }
+}
+
 func main() {
-    http.HandleFunc("/", miscHandler)
-    http.HandleFunc("/v2.0/s3tokens", tokenHandler)
+    hc := makeHTTPIface()
+    // Administration interface
+    http.HandleFunc("/api/v1/users", hc.userHandler)
+    http.HandleFunc("/api/v1/projects", hc.projectHandler)
+    http.HandleFunc("/api/v1/keys", hc.keyHandler)
+    // Multi-cluster sync interface
+    http.HandleFunc("/api/v1/sync", hc.cacheSyncHandler)
+    // Token validation interface
+    http.HandleFunc("/", hc.miscHandler)
+    http.HandleFunc("/v2.0/s3tokens", hc.tokenHandler)
     http.ListenAndServe(":8080", nil)
 }
 
-func miscHandler(w http.ResponseWriter, req *http.Request) {
+func (hc *httpIface) decode(req *http.Request, into interface{}) error {
+    defer req.Body.Close()
+    body, err := ioutil.ReadAll(req.Body)
+    if err != nil {
+        return err
+    }
+    fmt.Println("PROJECT", into)
+    err = json.Unmarshal(body, into)
+    if err != nil {
+        return err
+    }
+    return nil
+}
+
+func (hc *httpIface) projectHandler(w http.ResponseWriter, req *http.Request) {
+    switch req.Method {
+        case "GET":
+            // NOT IMPLEMENTED
+            break
+        case "POST":
+            ph := ""
+            var p = project{Project: &ph}
+            err := hc.decode(req, &p)
+            if err != nil {
+                fmt.Println(err)
+            }
+            if p.Project == nil {
+                // MISSING PROJEàCT
+                w.WriteHeader(http.StatusBadRequest)
+                return
+            }
+            fmt.Println(hc.rep(*p.Project))
+            err = hc.PC.ContainerCreate(hc.rep(*p.Project))
+            fmt.Println(err)
+
+        case "DELETE":
+            // NOT IMPLEMENTED
+            break
+    }
+}
+
+func (hc *httpIface) userHandler(w http.ResponseWriter, req *http.Request) {
+    switch req.Method {
+        case "GET":
+            // NOT IMPLEMENTED
+            break
+        case "POST":
+            ph := ""
+            u := user{Project: &ph}
+            hc.decode(req, &u)
+            fmt.Println(u)
+            if u.Project == nil && u.User == nil {
+                // MISSING PROJECT
+                w.WriteHeader(http.StatusBadRequest)
+                return
+            }
+            err := hc.PC.ObjectCreate(hc.rep(*u.Project), hc.rep(*u.User))
+            fmt.Println(err)
+        case "DELETE":
+            // NOT IMPLEMENTED
+            break
+    }
+}
+
+func (hc *httpIface) keyHandler(w http.ResponseWriter, req *http.Request) {
+    switch req.Method {
+        case "GET":
+            // NOT IMPLEMENTED
+            break
+        case "POST":
+            ph := ""
+            u := user{Project: &ph}
+            hc.decode(req, &u)
+            if u.Project == nil && u.User == nil {
+                // MISSING PROJECT
+                w.WriteHeader(http.StatusBadRequest)
+                return
+            }
+            access, secret := hc.newToken(*u.Project, *u.User)
+            err := hc.PC.ObjectSet(*u.Project, *u.User, map[string]string{access: secret})
+            if err != nil {
+                // Something wrong!
+                w.WriteHeader(http.StatusBadRequest)
+                return
+            }
+            data, err := json.Marshal(key{
+                Project: u.Project,
+                User: u.User,
+                Access: &access,
+                Secret: &secret,
+            })
+            fmt.Fprintf(w, string(data))
+        case "DELETE":
+            // NOT IMPLEMENTED
+            break
+    }
+}
+
+func (hc *httpIface) rep(entity string) string {
+    return hex.EncodeToString(signSHA256(seed, []byte(entity)))[:8]
+}
+
+func (hc *httpIface) newToken(project, user string) (string, string) {
+    // TOKEN SPEC:
+    // ACCESS
+    // total = 32 bytes
+    // oio = static
+    // version = 1 byte: token version
+    // rand1 = 4 bytes: random data
+    // project = 8 bytes: sha256, first 8 bytes
+    // user = 8 bytes: sha256, first 8 bytes
+    // rand2 = 8 bytes: random data
+    // SECRET
+    // total = 32 bytes
+    // rand1 = 32 bytes: random data
+
+    // Re-seed
+    rand.Seed(time.Now().UnixNano())
+    rand1 := make([]byte, 32)
+    rand2 := make([]byte, 32)
+    secret := make([]byte, 32)
+    rand.Read(rand1)
+    rand.Read(rand2)
+    rand.Read(secret)
+    return "oio" + acVersion + hex.EncodeToString(rand1)[:4] + hc.rep(project) + hc.rep(user) + hex.EncodeToString(rand2)[:8],
+        hex.EncodeToString(secret)[:32]
+}
+
+func (hc *httpIface) cacheSyncHandler(w http.ResponseWriter, req *http.Request) {
+    fmt.Println("cache sync")
+}
+
+
+func (hc *httpIface) miscHandler(w http.ResponseWriter, req *http.Request) {
+    // TODO: NOT IMPLEMENTED
     fmt.Println("test")
 }
 
-func tokenHandler(w http.ResponseWriter, req *http.Request) {
+func (hc *httpIface) tokenHandler(w http.ResponseWriter, req *http.Request) {
     var projectID = "Project ID whatever"
     var project = "demo"
     var user = "demo"
--- test.py
+++ test.py
@@ -1,39 +1,48 @@
 import requests
 import json
 
-url = "http://10.10.10.11:6006/v3.0/OPENIO/"
+url = "http://localhost:8080"
 
-res = requests.post(url + "container/create", params=dict(acct="IAM", ref="container2"))
-
+res = requests.post(url + "/api/v1/projects", data=json.dumps(dict(project="biloute")))
+print(res.__dict__)
+res = requests.post(url + "/api/v1/users", data=json.dumps(dict(project="biloute", user="test")))
+print(res.__dict__)
+res = requests.post(url + "/api/v1/keys", data=json.dumps(dict(project="biloute", user="test")))
 print(res.__dict__)
 
-# res2 = requests.post(url + "content/prepare", params=dict(acct="IAM", ref="container2", path="object"), data="{\"size\":0}")
+# url = "http://10.10.10.11:6006/v3.0/OPENIO/"
+#
+# res = requests.post(url + "container/create", params=dict(acct="IAM", ref="container2"))
+#
+# print(res.__dict__)
+#
+# # res2 = requests.post(url + "content/prepare", params=dict(acct="IAM", ref="container2", path="object"), data="{\"size\":0}")
+# # print(res2.__dict__)
+#
+#
+# bogus_data="{\"chunks\":[{\"url\":\"\",\"pos\":\"0\",\"size\":0,\"hash\":\"00000000000000000000000000000000\"}]}"
+#
+# res3 = requests.post(url + "content/create", params=dict(acct="IAM", ref="container2", path="object3"), data=bogus_data, headers={
+#     "x-oio-content-meta-length": "0",
+#     "x-oio-content-meta-policy": "SINGLE",
+#     "x-oio-content-meta-version": "1",
+#     "x-oio-content-meta-id": "AAAD"})
+#
+# print(res3.__dict__)
+#
+# print("\n\nSET/GET\n\n")
+#
+# # json.dumps([dict(
+# #         real_url="",
+# #         url="",
+# #         hash="a"*32,
+# #         pos="0.1",
+# #         size=0,
+# #     )])
+#
+# res2 = requests.post(url + "content/set_properties", params=dict(acct="IAM", ref="container2", path="object"),
+#     data=json.dumps({"properties": {"219021921020129121902":"32932039289328303290"}}))
 # print(res2.__dict__)
-
-
-bogus_data="{\"chunks\":[{\"url\":\"\",\"pos\":\"0\",\"size\":0,\"hash\":\"00000000000000000000000000000000\"}]}"
-
-res3 = requests.post(url + "content/create", params=dict(acct="IAM", ref="container2", path="object3"), data=bogus_data, headers={
-    "x-oio-content-meta-length": "0",
-    "x-oio-content-meta-policy": "SINGLE",
-    "x-oio-content-meta-version": "1",
-    "x-oio-content-meta-id": "AAAD"})
-
-print(res3.__dict__)
-
-print("\n\nSET/GET\n\n")
-
-# json.dumps([dict(
-#         real_url="",
-#         url="",
-#         hash="a"*32,
-#         pos="0.1",
-#         size=0,
-#     )])
-
-res2 = requests.post(url + "content/set_properties", params=dict(acct="IAM", ref="container2", path="object"),
-    data=json.dumps({"properties": {"219021921020129121902":"32932039289328303290"}}))
-print(res2.__dict__)
-
-res2 = requests.post(url + "content/get_properties", params=dict(acct="IAM", ref="container2", path="object"))
-print(res2.__dict__)
+#
+# res2 = requests.post(url + "content/get_properties", params=dict(acct="IAM", ref="container2", path="object"))
+# print(res2.__dict__)
